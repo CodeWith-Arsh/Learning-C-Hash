3050d348366d9fa824c192ec93dede4c64bf3360
arshid ahmad <arshid.ahmad1@gmail.com>
2024-07-09 11:31:59 +0530

    my First Commit

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -46,3 +46,49 @@ using System.ComponentModel.DataAnnotations;
     }
 }*/
 
+//Class Members
+/*namespace MyApplication
+{
+    class Car
+    {
+        string color = "red";
+        int maxSpeed = 5;
+
+        static void Main(string[] args)
+        {
+            Car myObj = new Car();
+            myObj.color = "red";
+            myObj.maxSpeed = 5;
+            Console.WriteLine(myObj.color);
+            Console.WriteLine(myObj.maxSpeed);
+        }
+    }
+}*/
+
+//Multiple Objects of One Class
+namespace MyApplication
+{
+    class Car
+    {
+        string model;
+        string color;
+        int year;
+
+        static void Main(string[] args)
+        {
+            Car Ford = new Car();
+            Ford.model = "Mustang";
+            Ford.color = "red";
+            Ford.year = 2004;
+
+            Car Maruti = new Car();
+            Maruti.model = "Maruti 800";
+            Maruti.color = "Blue";
+            Maruti.year = 2001;
+
+            Console.WriteLine(Maruti.model);
+            Console.WriteLine(Ford.color);
+        }
+    }
+}
+
