a33647fb98e4a6e444b788456ebf49e3eb9d4b11
arshid ahmad <arshid.ahmad1@gmail.com>
2024-07-09 12:23:38 +0530

    Using COnstructor in second file

--- ConsoleApp1/Prog2.cs
+++ ConsoleApp1/Prog2.cs
@@ -1,9 +1,26 @@
-﻿/*using System;
-
+﻿using System;
+using System.Xml.Schema;
 namespace MyApplication
 {
-    class Car
+    //Class in 2nd File
+    /*namespace MyApplication
     {
-        public string color = "red";
-    }
-}*/
+        class Car
+        {
+            public string color = "red";
+        }
+    }*/
+
+    /*class Program
+    {
+        static void Main(string[] args)
+        {
+            Car Ford = new Car("Mustang", "Red", 1969);
+            Car Opel = new Car("Astra", "White", 2005);
+
+            Console.WriteLine(Ford.model);
+            Console.WriteLine(Opel.model);
+        }
+    }*/
+
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,116 +1,196 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
 
-/*namespace MyApplication
+namespace MyApplication
 {
-    class Car
-    {
-        string color = "red";
-        static void Main (string[] args)
+    /*{
+        class Car
         {
-            Car car = new Car();
-            car.color = "red";
-            Console.WriteLine(car.color);
+            string color = "red";
+            static void Main(string[] args)
+            {
+                Car car = new Car();
+                car.color = "red";
+                Console.WriteLine(car.color);
+            }
         }
     }
-}*/
+    */
+    //Two Objects
+    /*{
+        class Car
+        {
+            string color = "red";
+            static void Main(string[] args)
+            {
+                Car car = new Car();
+                Car car2 = new Car();
+                car.color = "red";
+                car2.color = "green";
+                Console.WriteLine(car.color);
+                Console.WriteLine(car2.color);
+            }
+        }
+    }*/
 
-//Two Objects
-/*namespace MyApplication
-{
-    class Car
+    //Using Multiple Classes
+    /*namespace MyApplication
     {
-        string color = "red";
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Car MyObj = new Car();
+                Console.WriteLine(MyObj.color);
+            }
+        }
+    }*/
+
+    //Class Members
+    /*namespace MyApplication
+    {
+        class Car
+        {
+            string color = "red";
+            int maxSpeed = 5;
+
+            static void Main(string[] args)
+            {
+                Car myObj = new Car();
+                myObj.color = "red";
+                myObj.maxSpeed = 5;
+                Console.WriteLine(myObj.color);
+                Console.WriteLine(myObj.maxSpeed);
+            }
+        }
+    }*/
+
+    //Multiple Objects of One Class
+    /*namespace MyApplication
+    {
+        class Car
+        {
+            string model;
+            string color;
+            int year;
+
+            static void Main(string[] args)
+            {
+                Car Ford = new Car();
+                Ford.model = "Mustang";
+                Ford.color = "red";
+                Ford.year = 2004;
+
+                Car Maruti = new Car();
+                Maruti.model = "Maruti 800";
+                Maruti.color = "Blue";
+                Maruti.year = 2001;
+
+                Console.WriteLine(Maruti.model);
+                Console.WriteLine(Ford.color);
+            }
+        }
+    }
+    */
+
+    //C# Methods
+    /*namespace MyApplication
+    {
+        class Car
+        {
+            string color = "red";
+            string maxSpeed = "100";
+
+            public void fullThrottle()
+            {
+                Console.WriteLine("The car is going very fast with a speed of");
+
+            }
+            static void Main(string[] args)
+            {
+                Car car = new Car();
+                car.fullThrottle();
+            }
+        }
+    }*/
+
+    //Constructors
+    /*namespace MyApplication
+    {
+        class Car
+        {
+            public string model;
+            public string name;
+
+            public Car()
+            {
+                model = "Mustang";
+                name = "Racing Car";
+            }
+
+            static void Main(string[] args)
+            {
+                Car car = new Car();
+                Console.WriteLine(car.model);
+            }
+        }
+    }*/
+
+    //Constructor Parameters
+    /*class Car
+    {
+        public string model;
+
+        public Car(string modelName)
+        {
+            model = modelName;
+        }
+
         static void Main(string[] args)
         {
-            Car car = new Car();
-            Car car2 = new Car();
-            car.color = "red";
-            car2.color = "green";
-            Console.WriteLine(car.color);
-            Console.WriteLine(car2.color);
+            Car Ford = new Car("Mustang");
+            Console.Write(Ford.model);
         }
-    }
-}*/
+    }*/
 
-//Using Multiple Classes
-/*namespace MyApplication
-{
-    class Program
+    /*  class Car
+      {
+          public string model;
+          public string color;
+          public int year;
+
+          public Car(string modelName, string modelColor, int modelYear)
+          {
+              model = modelName;
+              color = modelColor;
+              year = modelYear;
+          }
+
+          static void Main(string[] args)
+          {
+              Car Ford = new Car("Mustang","Red",2022);
+              Console.WriteLine(Ford.model+ " AND "+ Ford.color);
+          }
+      }*/
+
+    //Using Constructor in Two Files prog2.cs
+
+   /* class Car
     {
-        static void Main(string[] args)
+        public string model;
+        public string color;
+        public int year;
+
+        public Car(string modelName, string modelColor, int modelYear)
         {
-            Car MyObj = new Car();
-            Console.WriteLine(MyObj.color);
+            model = modelName;
+            color = modelColor;
+            year = modelYear;
         }
-    }
-}*/
+    }*/
 
-//Class Members
-/*namespace MyApplication
-{
-    class Car
-    {
-        string color = "red";
-        int maxSpeed = 5;
 
-        static void Main(string[] args)
-        {
-            Car myObj = new Car();
-            myObj.color = "red";
-            myObj.maxSpeed = 5;
-            Console.WriteLine(myObj.color);
-            Console.WriteLine(myObj.maxSpeed);
-        }
-    }
-}*/
 
-//Multiple Objects of One Class
-/*namespace MyApplication
-{
-    class Car
-    {
-        string model;
-        string color;
-        int year;
 
-        static void Main(string[] args)
-        {
-            Car Ford = new Car();
-            Ford.model = "Mustang";
-            Ford.color = "red";
-            Ford.year = 2004;
 
-            Car Maruti = new Car();
-            Maruti.model = "Maruti 800";
-            Maruti.color = "Blue";
-            Maruti.year = 2001;
-
-            Console.WriteLine(Maruti.model);
-            Console.WriteLine(Ford.color);
-        }
-    }
 }
-*/
-
-//C# Methods
-/*namespace MyApplication
-{
-    class Car
-    {
-        string color = "red";
-        string maxSpeed = "100";
-
-        public void fullThrottle()
-        {
-            Console.WriteLine("The car is going very fast with a speed of");
-
-        }
-        static void Main(string[] args)
-        {
-            Car car = new Car();
-            car.fullThrottle();
-        }
-    }
-}*/
-
