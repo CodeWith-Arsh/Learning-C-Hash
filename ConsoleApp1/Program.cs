394b3287c968d7194e461661f4e1ca9bbc13b4f8
arshi <arshi@ARSH>
2024-07-09 11:13:31 +0530

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+/*namespace MyApplication
+{
+    class Car
+    {
+        string color = "red";
+        static void Main (string[] args)
+        {
+            Car car = new Car();
+            car.color = "red";
+            Console.WriteLine(car.color);
+        }
+    }
+}*/
+
+//Two Objects
+/*namespace MyApplication
+{
+    class Car
+    {
+        string color = "red";
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+            Car car2 = new Car();
+            car.color = "red";
+            car2.color = "green";
+            Console.WriteLine(car.color);
+            Console.WriteLine(car2.color);
+        }
+    }
+}*/
+
+//Using Multiple Classes
+/*namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car MyObj = new Car();
+            Console.WriteLine(MyObj.color);
+        }
+    }
+}*/
+
